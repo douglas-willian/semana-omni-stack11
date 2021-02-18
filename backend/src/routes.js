2d2cff3874f332cf78734248a027d3750a1418b7
douglas <doug.william@hotmail.com>
2021-02-18 01:01:55 -0300

    start new repo

---
+++ backend/src/routes.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const OngControlller = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
+const SessionController = require('./controllers/SessionController')
+const routes = express.Router()
+
+routes.post('/sessions', SessionController.create)
+
+routes.get('/ongs', OngControlller.index)
+routes.post('/ongs', OngControlller.create)
+
+routes.get('/incidents', IncidentController.index)
+routes.post('/incidents', IncidentController.create)
+routes.delete('/incidents/:id', IncidentController.delete)
+
+routes.get('/profile', ProfileController.index)
+
+module.exports = routes
